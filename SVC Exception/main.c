3f6fd05992e72e6b9322ad2c2faf7b9ed356bad6
Pranjal Shrivastava <52209005+pran005@users.noreply.github.com>
2021-02-17 01:56:51 +0530

    Update main.c

--- SVC Exception/main.c
+++ SVC Exception/main.c
@@ -1,4 +1,4 @@
-int __svc(0x00) service_Add (int a, int b) ;			// __svc is a compiler keyword that replaces a function call with an SVC instruction containing the specified number.
+int __svc(0x00) service_Add (int a, int b) ; // __svc is a compiler keyword that replaces a function call with an SVC instruction containing the specified number.
 int __svc(0x01) service_sub (int a, int b) ;
 int __svc(0x02) service_mult (int a, int b) ;
 int __svc(0x03) service_div (int a, int b) ;
@@ -7,14 +7,14 @@ void SVC_Handler_C(unsigned int *svc_args) ;
 int x,y,z ; 
 int main()
 {
-	x= 1 ; 
-	y= 5 ; 
-	z= service_Add(x,y) ;  
+    x= 1 ; 
+    y= 5 ; 
+    z= service_Add(x,y) ;  
 
 	x = 9 ;
 	y= 2 ; 
 	z = service_sub(x,y) ; 
-	
+
 	x=3;
 	y=4 ; 
 	z = service_mult(x,y) ;
@@ -29,9 +29,9 @@ __asm void SVC_Handler(void)
 	TST LR,#4 // check bit 2
 		
 	ITE EQ 
-	MRSEQ R0,MSP					// if bit 2 is 0 	
-	MRSNE R0,PSP				 // if bit 2 is not 0 
-	B 		__cpp(SVC_Handler_C) 
+	MRSEQ R0,MSP				// if bit 2 is 0 	
+	MRSNE R0,PSP    	       // if bit 2 is not 0 
+	B __cpp(SVC_Handler_C) 
 
 }
 
